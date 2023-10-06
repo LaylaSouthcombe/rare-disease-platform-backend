e352a9b239e233c274005d540d2274643fa07486
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-10-06 22:32:20 +0100

    add accounts and rename patients and rare diseases

--- RareDiseasePlatform/rare_diseases/apps.py
+++ RareDiseasePlatform/rare_diseases/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RarediseaseConfig(AppConfig):
+class RareDiseasesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'RareDisease'
+    name = 'rare_diseases'
