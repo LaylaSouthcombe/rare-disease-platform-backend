c0f78084156f00344cfa77865a9b31aaba412d5e
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-12-14 13:51:23 +0000

    add new models

--- RareDiseasePlatform/accounts/views.py
+++ RareDiseasePlatform/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Account
 
 # Create your views here.
+def home(request):
+    accounts = Account.objects.all()
+    print(accounts)
+    return HttpResponse("hello world")
