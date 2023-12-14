918e68e6212c7ac55b75fbda5fa9b79295cf3adb
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-12-14 14:34:22 +0000

    add account and clinician model

---
+++ Models/Account.cs
@@ -0,0 +1,8 @@
+namespace Models
+{
+    public class Account {
+        public int id {get; set;}
+        public string email {get; set;}
+        public string password {get; set;}
+    }
+}
---
+++ Models/Clinician.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public int id { get; set; }
+    public string salutation { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+    public string specialty { get; set; }
+}
